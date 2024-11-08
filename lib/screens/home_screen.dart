00c8f9d056e99750f671eb0ea9143acc45ffbaa6
Aliosa06 <aliusfiodorovas@gmail.com>
2024-11-08 20:21:48 +0200

    First try

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(child: Text('Hello Home Screen'),),
+          Container(child: Text('Hello How r u'),)
+        ],
+      ),
+    );
+  }
+}
